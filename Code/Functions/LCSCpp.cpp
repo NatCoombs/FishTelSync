91829ea03af507fad6e2e64a240acbdd6b9c84c3
NatCoombs <natcoombs@me.com>
2021-10-27 16:09:40 -0500

    Updating pure C++ LCS tool. Still throwing a segfault somewhere

--- Code/Functions/LCSCpp.cpp
+++ Code/Functions/LCSCpp.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string.h>
 #include <vector>
+#include <cstring>
 
 int lcslen = 0;
 
@@ -25,10 +26,32 @@ int DPmat[1000][1000];
 std::vector<std::string> data;
 std::vector<std::string> F1Index;
 std::vector<std::string> F2Index;
-// std::vector<std::string> Fish2Seq;
-// std::vector<std::string> Fish2Seq;
+int StringIndex = 0;
+std::vector<std::string> Fish1Seq;
+std::vector<std::string> Fish2Seq;
 std::vector<std::vector<std::vector<std::string>>> PullSet;
 
+void StringVecBuilder(char** argv, int Start, std::string BreakVal, int StringIndex, std::vector<std::string> &Fish1Seq, std::vector<std::string> &Fish2Seq){
+    if(StringIndex == 0){
+        if(argv[Start]==BreakVal){
+        StringIndex = StringIndex + 1;
+        StringVecBuilder(argv, Start + 1, "Switch", StringIndex, Fish1Seq, Fish2Seq);
+        return;
+    } else{
+        Fish1Seq.push_back(argv[Start]);
+        StringVecBuilder(argv, Start + 1, "Switch", StringIndex, Fish1Seq, Fish2Seq);
+        return;
+    }
+    } else{
+        if(argv[Start]==BreakVal){
+        return;
+    } else{
+        Fish2Seq.push_back(argv[Start]);
+        StringVecBuilder(argv, Start + 1, "Switch", StringIndex, Fish1Seq, Fish2Seq);
+        return;
+    }
+    }
+}
 void DPmatInit(int Size1, int Size2){
 
         int DPmat[Size1+1][Size2+1];
@@ -82,7 +105,7 @@ int lcscore(std::vector<std::string> Fish1Seq, std::vector<std::string> Fish2Seq
 
 }
 
-void PullAll(std::vector<std::string> Fish1Seq, std::vector<std::string> Fish2Seq, int Size1, int Size2, std::vector<std::string> data, int indx1, int indx2, int currlcs){
+void PullAll(std::vector<std::string> Fish1Seq, std::vector<std::string> Fish2Seq, int Size1, int Size2, std::vector<std::string> data, int indx1, int indx2, int currlcs, std::vector<std::vector<std::vector<std::string>>> PullSet){
 
     // if currlcs is equal to lcslen then staple it to the output
         if (currlcs == lcslen)
@@ -118,7 +141,7 @@ void PullAll(std::vector<std::string> Fish1Seq, std::vector<std::string> Fish2Se
                                                     data[currlcs] = Fish1Seq[i];
                                                       F1Index[currlcs] = std::to_string(i);
                                                       F2Index[currlcs] = std::to_string(j);
-                                                    PullAll(Fish1Seq, Fish2Seq, Size1, Size2, data, i+1, j+1, currlcs+1);
+                                                    PullAll(Fish1Seq, Fish2Seq, Size1, Size2, data, i+1, j+1, currlcs+1, PullSet);
                                                     break;
                                                   }
                                   
@@ -126,7 +149,7 @@ void PullAll(std::vector<std::string> Fish1Seq, std::vector<std::string> Fish2Se
             }
 }
 
-/*std::vector<std::vector<std::vector<std::string>>>*/ void FullLCSExtractor(std::vector<std::string> Fish1Seq, std::vector<std::string> Fish2Seq){
+/*std::vector<std::vector<std::vector<std::string>>>*/ void FullLCSExtractor(std::vector<std::string> Fish1Seq, std::vector<std::string> Fish2Seq, int argc, char** argv){
     // Clone is used to avoid memory issues
 
   
@@ -161,25 +184,35 @@ void PullAll(std::vector<std::string> Fish1Seq, std::vector<std::string> Fish2Se
     std::vector<std::string> F2Index(lcslen);
     
     
-    std::vector<std::vector<std::vector<std::string>>> PullSet; // Trying a vector of vectors of vectors of strings
+    //std::vector<std::vector<std::vector<std::string>>> PullSet; // Trying a vector of vectors of vectors of strings
 
-    PullAll(Fish1Seq, Fish2Seq, Size1, Size2, data, 0, 0, 0);
+    PullAll(Fish1Seq, Fish2Seq, Size1, Size2, data, 0, 0, 0, PullSet);
     
-    std::cout << PullSet[0][0][0] << PullSet[0][0][1] << PullSet[0][0][2] << PullSet[0][0][3] << "\n";
-    std::cout << PullSet[0][1][0] << PullSet[0][1][1] << PullSet[0][1][2] << PullSet[0][1][3] << "\n";
-    std::cout << PullSet[0][2][0] << PullSet[0][2][1] << PullSet[0][2][2] << PullSet[0][2][3] << "\n";
-
-
+    // std::cout << PullSet[0][0][0] << PullSet[0][0][1] << PullSet[0][0][2] << PullSet[0][0][3] << "\n";
+    // std::cout << PullSet[0][1][0] << PullSet[0][1][1] << PullSet[0][1][2] << PullSet[0][1][3] << "\n";
+    // std::cout << PullSet[0][2][0] << PullSet[0][2][1] << PullSet[0][2][2] << PullSet[0][2][3] << "\n";
+    if(strcmp(argv[argc-1], "Debug")==0){
+    for(int k = 0;k < (lcslen-1); k++){
+        std::cout << PullSet[0][0][k];
+    }
+    std::cout << "\n";
+    
+    for(int k = 0;k < (lcslen-1); k++){
+        std::cout << PullSet[0][1][k];
+    }
+    std::cout << "\n";
+    
+    for(int k = 0;k < (lcslen-1); k++){
+        std::cout << PullSet[0][2][k];
+    }
+    std::cout << "\n";
+    }
+    
     return;// PullSet;
 }
 
-int main(std::vector<std::string> Fish1Seq, std::vector<std::string> Fish2Seq){
-    // std::vector<std::string>> Fish1Seq;
-    // std::vector<std::string>> Fish2Seq;
-    // std::cout << "Please input one sequence vector, then a second";
-    // std::cin >> Fish1Seq;
-    // std::cin >> Fish2Seq;
-    FullLCSExtractor(Fish1Seq, Fish2Seq);
+int main(int argc, char** argv){
+    StringVecBuilder(argv, 1, "Switch", StringIndex, Fish1Seq, Fish2Seq);
+    FullLCSExtractor(Fish1Seq, Fish2Seq, argc, argv);
     return 0;
 }
-
